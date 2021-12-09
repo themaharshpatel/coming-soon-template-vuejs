33ae03d22ef208b1ba202d1f263063ec700ce75e
Maharsh Patel <49917427+themaharshpatel@users.noreply.github.com>
2021-12-10 02:44:04 +0530

    Changes For my Portfolio

--- src/components/TemplateOne.vue
+++ src/components/TemplateOne.vue
@@ -1,7 +1,7 @@
 <template>
   <b-row no-gutters class="h-100 template-one">
     <b-col cols="12" sm="6" xl="8" class="p-5 d-none d-sm-flex justify-content-center align-items-center background-container" :style="{backgroundImage:'url('+backgroundImage+')'}"></b-col>
-    <b-col cols="12" sm="6" xl="4" class="bg-light p-5 d-flex flex-column justify-content-center align-items-center shadow-lg">
+    <b-col cols="12" sm="6" xl="4" class="bg-light p-3 d-flex flex-column justify-content-center align-items-center shadow-lg">
       <div class="text-center my-auto">
         <img :src="logo" alt="logo" class="img-fluid mx-auto mb-4">
         <div class="title mb-3">{{ title }}</div>
@@ -20,6 +20,10 @@
         <a v-if="instagram" :href="instagram" target="_blank" class="text-body mx-2 my-3">Instagram</a>
         <a v-if="youtube" :href="youtube" target="_blank" class="text-body mx-2 my-3">YouTube</a>
         <a v-if="linkedin" :href="linkedin" target="_blank" class="text-body mx-2 my-3">LinkedIn</a>
+        <a v-if="mail" :href="'mailto:'+mail" target="_blank" class="text-body mx-2 my-3">Email</a>
+      </div>
+      <div class="mt-auto text-center social-media-container">
+        <a href="https://github.com/kgkanatsios/coming-soon-template-vuejs" target="_blank" class="text-body mx-2 my-6">Website credits @kgkanatsios</a>
       </div>
     </b-col>
   </b-row>
@@ -31,13 +35,14 @@ export default {
   data(){
     return {
       logo: require('@/assets/logo.png'),
-      backgroundImage: require('@/assets/bg.jpg'),
+      backgroundImage: require('@/assets/bg.svg'),
       title: process.env.VUE_APP_TITLE,
       disclaimer:[process.env.VUE_APP_DISCLAIMER_FIRST_LINE, process.env.VUE_APP_DISCLAIMER_SECOND_LINE],
       facebook:process.env.VUE_APP_FACEBOOK,
       instagram:process.env.VUE_APP_INSTAGRAM,
       youtube:process.env.VUE_APP_YOUTUBE,
-      linkedin:process.env.VUE_APP_LINKEDIN
+      linkedin:process.env.VUE_APP_LINKEDIN,
+      mail:process.env.VUE_APP_MAIL
     }
   }
 }
